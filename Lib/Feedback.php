64d8f69afdb421a99c8ed42877bdbbc45ebc39a3
xionggang <xionggang@126.com>
2016-08-17 12:34:23 +0800

    all file

---
+++ Lib/Feedback.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * 获取失效的regId列表.
+ * @author wangkuiwei
+ * @name Feedback
+ * @desc 获取失效的regId列表。
+ *
+ */
+namespace xmpush\lib;
+
+use xmpush\Lib\HttpBase;
+
+class Feedback extends HttpBase {
+	
+	public function __construct(){
+		parent::__construct();
+	}
+	
+        public function getInvalidRegIds($retries=1){
+		$url = Constants::fetch_invalid_regids_url;
+		$result = $this->getResult($url, array(), $retries);
+		return $result;
+	}
+	
+}
+
+?>
